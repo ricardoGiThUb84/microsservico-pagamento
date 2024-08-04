8ebfdf087bf89cc5fb01c682ef8bf1e72cde6a5f
ricardo <rosastaricardo84@gmail.com>
2024-08-04 10:11:50 -0300

    config rabbit template

--- pedidos/src/main/java/br/com/alurafood/pedidos/amqp/PagamentosListener.java
+++ pedidos/src/main/java/br/com/alurafood/pedidos/amqp/PagamentosListener.java
@@ -1,2 +1,18 @@
-package br.com.alurafood.pedidos.amqp;public class PagamentosListener {
+package br.com.alurafood.pedidos.amqp;
+
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PagamentosListener {
+
+    private final String NOME_FILA_PAGAMENTO = "pagamento.concluido";
+
+    @RabbitListener(queues = NOME_FILA_PAGAMENTO)
+    public void recieveMessage(Message message){
+
+        System.out.println(String.format("Message recieved: %s", message.toString()));
+    }
 }
